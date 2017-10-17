f7456c890af60407ea5b37a5d60812c4723f3caa
SayliS <kalin.venkov@eldersoss.com>
2017-10-17 16:13:31 +0300

    Batman commit

---
+++ build-configuration.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+%FAKE% %NYX% "target=clean" -st
+%FAKE% %NYX% "target=RestoreNugetPackages" -st
+
+IF NOT [%1]==[] (set RELEASE_NUGETKEY="%1")
+IF NOT [%2]==[] (set RELEASE_TARGETSOURCE="%2")
+
+SET SUMMARY="Localization Abstractions"
+SET DESCRIPTION="Localization Abstractions"
+
+SET SUMMARY_PHRASEAPP="PhraseApp Localization"
+SET DESCRIPTION_PHRASEAPP="PhraseApp Localization"
+
+%FAKE% %NYX% appName=Localization.Abstractions          appSummary=%SUMMARY% appDescription=%DESCRIPTION% nugetserver=%NUGET_SOURCE_DEV_PUSH% nugetkey=%RELEASE_NUGETKEY%
+%FAKE% %NYX% appName=Localization.PhraseApp             appSummary=%SUMMARY_PHRASEAPP% appDescription=%DESCRIPTION_PHRASEAPP% nugetserver=%NUGET_SOURCE_DEV_PUSH% nugetkey=%RELEASE_NUGETKEY%
