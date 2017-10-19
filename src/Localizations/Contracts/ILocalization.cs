6fcbda333316bbd14a62d0fcd494db6906e6c17d
SayliS <kalin.venkov@eldersoss.com>
2017-10-19 12:04:03 +0300

    Adds ability to specify default locale
    Adds ability to specify strict locale e.g en-GB would fall back to en on failure and strict locale set to false

--- src/Localizations/Contracts/ILocalization.cs
+++ src/Localizations/Contracts/ILocalization.cs
@@ -3,5 +3,19 @@
     public interface ILocalization
     {
         SafeGet<TranslationModel> Get(string key, string locale);
+
+        /// <summary>
+        /// Specifies if fall back to two letter part of locale is allowed e.g en-GB would fall back to en
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        ILocalization UseStrictLocale(bool value);
+
+        /// <summary>
+        /// Specifies default fall back locale
+        /// </summary>
+        /// <param name="locale"></param>
+        /// <returns></returns>
+        ILocalization UseDefaultLocale(string locale);
     }
 }
--- src/Localizations/Localizations.rn.md
+++ src/Localizations/Localizations.rn.md
@@ -1,3 +1,7 @@
+#### 0.1.2 - 19.10.2017
+* Adds ability to specify default locale
+* Adds ability to specify strict locale e.g en-GB would fall back to en on failure and strict locale set to false
+
 #### 0.1.1 - 18.10.2017
 * Adds ability to specify LastModified date for translation
 
