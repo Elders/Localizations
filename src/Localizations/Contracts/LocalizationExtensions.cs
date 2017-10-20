03bd765ff403bfe25fadcedd267eb498551b4b34
SayliS <kalin.venkov@eldersoss.com>
2017-10-20 10:23:09 +0300

    Adds localization extensions

---
+++ src/Localizations/Contracts/LocalizationExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Localizations.Contracts
+{
+    public static class LocalizationExtensions
+    {
+        public static string GetValue(this ILocalization localization, string key, string locale, string defaultValue)
+        {
+            var translation = localization.Get(key, locale);
+
+            if (translation.Found == true)
+                return translation.Result().Value;
+
+            return defaultValue;
+        }
+
+        public static string GetValue(this ILocalization localization, string key, string locale)
+        {
+            var translation = localization.Get(key, locale);
+
+            if (translation.Found == true)
+                return translation.Result().Value;
+
+            return $"missing-key-'{key}'-locale-'{locale}'";
+        }
+    }
+}
