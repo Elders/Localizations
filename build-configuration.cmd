cae9eb31be3481a6de5bf4d964797f955ef52884
SayliS <rssaylis@gmail.com>
2018-09-30 20:30:11 +0300

    Updates build scripts

--- build-configuration.cmd
+++ build-configuration.cmd
@@ -1,17 +1,4 @@
 @echo off
 
-%FAKE% %NYX% "target=clean" -st
-%FAKE% %NYX% "target=RestoreNugetPackages" -st
-
-IF NOT [%1]==[] (set RELEASE_NUGETKEY="%1")
-IF NOT [%2]==[] (set RELEASE_TARGETSOURCE="%2")
-
-SET SUMMARY="Localization Abstractions"
-SET DESCRIPTION="Localization Abstractions"
-
-SET SUMMARY_PHRASEAPP="PhraseApp Localization"
-SET DESCRIPTION_PHRASEAPP="PhraseApp Localization"
-
-%FAKE% %NYX% appName=Localizations                       appSummary=%SUMMARY% appDescription=%DESCRIPTION% nugetserver=%NUGET_SOURCE_DEV_PUSH% nugetkey=%RELEASE_NUGETKEY%
-
-%FAKE% %NYX% appName=Localizations.PhraseApp             appSummary=%SUMMARY_PHRASEAPP% appDescription=%DESCRIPTION_PHRASEAPP% nugetserver=%NUGET_SOURCE_DEV_PUSH% nugetkey=%RELEASE_NUGETKEY%
+@powershell -File .nyx\build.ps1 '--appname=Localizations' '--nugetPackageName=Localizations'
+@powershell -File .nyx\build.ps1 '--appname=Localizations.PhraseApp' '--nugetPackageName=Localizations.PhraseApp'
