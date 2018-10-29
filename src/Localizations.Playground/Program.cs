cef821de1abfc5d2c643398d9c3c9bca182516d4
SayliS <kalin.venkov@eldersoss.com>
2018-10-29 10:32:29 +0200

    Adds playground

---
+++ src/Localizations.Playground/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Localizations.PhraseApp;
+
+namespace Localizations.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan ttl = TimeSpan.FromMinutes(1);
+            string accessToken = "";
+            string projectId = "";
+            ILocalization localization = new PhraseAppLocalization(accessToken, projectId, ttl);
+
+            var byKey = localization.Get("1vipcustomer", "En");
+            var byKeyWithHeader = localization.Get("1vipcustomer", new AcceptLanguageHeader("EN"));
+            var getAll = localization.GetAll("eN");
+            var getAllWithHeader = localization.GetAll(new AcceptLanguageHeader("EN"));
+        }
+    }
+}
