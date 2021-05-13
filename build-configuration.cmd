bfd3030edf3cbf99a702b6f0c5ab98d37e3fb0f3
gshukov98 <56069656+gshukov98@users.noreply.github.com>
2021-05-13 15:55:44 +0300

    Switches to azure-pipelines and sem-rel

--- build-configuration.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-@powershell -File .nyx\build.ps1 '--appname=Localizations' '--nugetPackageName=Localizations'
-@powershell -File .nyx\build.ps1 '--appname=Localizations.PhraseApp' '--nugetPackageName=Localizations.PhraseApp'
